b2fffad50c5671274f2ac30ed6a530f357fdb2a3
Elias Nepomuceno <eliasnepo12@gmail.com>
2021-08-05 11:45:41 -0300

    Adiciona endpoint para deletar autor e query params para email

--- src/main/kotlin/br/com/zup/autores/BuscaDetalheDeAutoresController.kt
+++ src/main/kotlin/br/com/zup/autores/BuscaDetalheDeAutoresController.kt
@@ -3,16 +3,24 @@ package br.com.zup.autores
 import io.micronaut.http.HttpResponse
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Get
+import io.micronaut.http.annotation.QueryValue
 
 @Controller("/autores")
 class BuscaDetalheDeAutoresController(val autorRepository: AutorRepository) {
 
     @Get
-    fun lista(): HttpResponse<List<DetalhesDoAutorResponse>> {
-        val autores = autorRepository.findAll()
+    fun lista(@QueryValue(defaultValue = "") email: String): HttpResponse<Any> {
+        if (email.isBlank()) {
+            val autores = autorRepository.findAll()
+            val resposta = autores.map { autor -> DetalhesDoAutorResponse(autor) }
+            return HttpResponse.ok(resposta)
+        }
 
-        val resposta = autores.map { autor -> DetalhesDoAutorResponse(autor) }
+        val possivelAutor = autorRepository.findByEmail(email)
+        if (possivelAutor.isEmpty) {
+            return HttpResponse.badRequest()
+        }
 
-        return HttpResponse.ok(resposta)
+        return HttpResponse.ok(DetalhesDoAutorResponse(possivelAutor.get()))
     }
 }
