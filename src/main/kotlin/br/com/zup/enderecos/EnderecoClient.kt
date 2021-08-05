f1f5e50f6e844d1e437664730a629ff795f12094
Elias Nepomuceno <eliasnepo12@gmail.com>
2021-08-05 15:34:08 -0300

    Adiciona suporte a consultar com formato XML

--- src/main/kotlin/br/com/zup/enderecos/EnderecoClient.kt
+++ src/main/kotlin/br/com/zup/enderecos/EnderecoClient.kt
@@ -1,6 +1,7 @@
 package br.com.zup.enderecos
 
 import io.micronaut.http.HttpResponse
+import io.micronaut.http.MediaType
 import io.micronaut.http.annotation.Get
 import io.micronaut.http.annotation.PathVariable
 import io.micronaut.http.client.annotation.Client
@@ -10,4 +11,7 @@ interface EnderecoClient {
 
     @Get("/ws/{cep}/json/")
     fun consulta(@PathVariable cep: String): HttpResponse<EnderecoResponse>
+
+    @Get(value = "/ws/{cep}/xml/", consumes = [MediaType.APPLICATION_XML])
+    fun consultaComXml(@PathVariable cep: String): HttpResponse<EnderecoResponse>
 }
